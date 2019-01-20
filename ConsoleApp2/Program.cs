5c780e8ccc24302bc056ec5b168874da43f57bea
shadrackkiprotich <mylesshadrack99@gmail.com>
2019-01-19 18:09:39 -0800

    ticket_selling

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿ using System;
+
+ using System.Security.Cryptography.X509Certificates;
+ using Microsoft.VisualBasic.CompilerServices;
+ 
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your name");
+            string f = Console.ReadLine();
+            Console.WriteLine("enter your category");
+            var d= Console.ReadLine();
+             
+            if (d.Contains("vip"))
+            {
+              Console .WriteLine("your paymen is equal to 2500");
+            }
+            else
+            {
+                Console.WriteLine("your paymet is equal to 1500");
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
